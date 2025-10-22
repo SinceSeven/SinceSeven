17305179004b1269a5832ad76b0b78eab13f5c18
Petar Shikrenov <shikrenov@proton.me>
2025-10-23 01:18:33 +0300

    fix: footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="bg-muted dark:bg-card border-t px-4 py-3">
-      <div className="mx-auto max-w-screen-xl text-center flex flex-col items-center gap-2">
+    <footer className="relative overflow-hidden border-t px-4 py-3">
+      <div className="mx-auto max-w-screen-xl text-center flex flex-col items-center gap-2 relative z-10">
         <p className="text-muted-foreground text-xs font-medium">
           &copy; {new Date().getFullYear()} SinceSeven. Всички права запазени.
         </p>
         <Link href="https://www.instagram.com/sincesevenbg/">
-          <Instagram className="text-muted-foreground h-4 w-4" />
+          <Instagram className="text-muted-foreground h-4 w-4 hover:text-primary transition-colors" />
         </Link>
       </div>
     </footer>
