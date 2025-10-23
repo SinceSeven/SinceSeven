28e37cb50ef0516ce60b35e84c25ad901eeaf03e
nikola88393 <nikola88393@gmail.com>
2025-10-24 00:34:31 +0300

    Add web app manifest and icons for SinceSeven

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="bg" suppressHydrationWarning>
+      <head>
+        <meta name="apple-mobile-web-app-title" content="SinceSeven" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
