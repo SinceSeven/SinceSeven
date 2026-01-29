59d1423b28af8988b77423e80aaefd2b33c4b1e3
nikola <nikola.a@appgr8.com>
2026-01-29 10:27:25 +0200

    Change back to script tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 import { Bricolage_Grotesque, Geist_Mono } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Bricolage_Grotesque({
@@ -28,15 +27,15 @@ export default function RootLayout({
     <html lang="bg" suppressHydrationWarning>
       <head>
         <meta name="apple-mobile-web-app-title" content="SinceSeven" />
+        <script
+          src="https://analytics.ahrefs.com/analytics.js"
+          data-key="+fKl1e8RrCrwFZQW5DmuuA"
+          async
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Script
-          src="https://analytics.ahrefs.com/analytics.js"
-          data-key="+fKl1e8RrCrwFZQW5DmuuA"
-          strategy="afterInteractive"
-        />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
